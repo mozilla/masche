4e1fe4eaac5737c21d81f273ce2a9ac52467a020
Marco Vanotti <mvanotti@dc.uba.ar>
2014-08-15 14:00:49 -0300

    Example Test

---
+++ list_libs_test.go
@@ -0,0 +1,17 @@
+package migmem
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+)
+
+func TestFindProcWithLib(t *testing.T) {
+	r, err := regexp.Compile("libc")
+	if err != nil {
+		t.Fatal(err)
+	}
+	
+	ls, err := FindProcWithLib(r)
+	fmt.Println(ls)
+}
