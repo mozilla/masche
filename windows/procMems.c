4c2b1f1ae1538b864119b2ab93d5584d6e648bdb
Marco Vanotti <mvanotti@dc.uba.ar>
2014-09-03 16:13:11 -0300

    Fix indentation.

--- windows/procMems.c
+++ windows/procMems.c
@@ -4,43 +4,43 @@
 #include <stdio.h>
 
 void printInfo(MEMORY_BASIC_INFORMATION info) {
-  printf("%p %p %d %llu %d %d %d\n", info.BaseAddress, info.AllocationBase,
-                                    info.AllocationProtect, info.RegionSize,
-                                    info.State, info.Protect, info.Type);
+    printf("%p %p %d %llu %d %d %d\n", info.BaseAddress, info.AllocationBase,
+            info.AllocationProtect, info.RegionSize,
+            info.State, info.Protect, info.Type);
 }
 
 int main(void) {
-  DWORD pid = 3308;
-  HANDLE hndl = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,
-					FALSE,
-					pid);
-	
-	if (hndl == NULL) {
-	  fprintf(stderr, "Error! %d", GetLastError());
-    exit(EXIT_FAILURE);
-	}
-	
-	MEMORY_BASIC_INFORMATION info;
-	LPCVOID addr = 0x0;
-  while (TRUE) {
-    SIZE_T res = VirtualQueryEx(hndl,
-                                addr,
-                                &info,
-                                sizeof(info));
-                                
-    if (res == 0) {
-      DWORD err = GetLastError();
-      if (err == ERROR_INVALID_PARAMETER) { 
-        // This means that the address we are using is invalid, i.e: no more addresses left!
-        break;
-      }
-      fprintf(stderr, "Error! %d", GetLastError());
-      exit(EXIT_FAILURE);
+    DWORD pid = 3308;
+    HANDLE hndl = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,
+            FALSE,
+            pid);
+
+    if (hndl == NULL) {
+        fprintf(stderr, "Error! %d", GetLastError());
+        exit(EXIT_FAILURE);
     }
-    
-    printInfo(info);
-    addr += info.RegionSize;
-  }
-  
-  return 0;
+
+    MEMORY_BASIC_INFORMATION info;
+    LPCVOID addr = 0x0;
+    while (TRUE) {
+        SIZE_T res = VirtualQueryEx(hndl,
+                addr,
+                &info,
+                sizeof(info));
+
+        if (res == 0) {
+            DWORD err = GetLastError();
+            if (err == ERROR_INVALID_PARAMETER) { 
+                // This means that the address we are using is invalid, i.e: no more addresses left!
+                break;
+            }
+            fprintf(stderr, "Error! %d", GetLastError());
+            exit(EXIT_FAILURE);
+        }
+
+        printInfo(info);
+        addr += info.RegionSize;
+    }
+
+    return 0;
 }
