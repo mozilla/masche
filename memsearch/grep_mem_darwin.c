4c9773107de42065f81b596a79c4279ca422ab5f
Patricio Palladino <email@patriciopalladino.com>
2014-09-24 19:05:38 -0300

    Better darwin's mem_search return values
    Improved the interface of the memory search C functions for OS X. Now
    there is a special struct that has address where the needle was found
    (or NULL), and any error that ocurred (fatal or not).

--- memsearch/grep_mem_darwin.c
+++ memsearch/grep_mem_darwin.c
@@ -3,88 +3,140 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
-//FIXME(alcuadrado): Are all these necessary?
-#include <mach/vm_map.h>
-#include <mach/message.h>
+#include <mach/mach.h>
 #include <mach/mach_vm.h>
-#include <mach-o/dyld_images.h>
 
 /**
- * This is the initial value will use for mach_vm_region_recurse, LLDB considers
- * (LLVM's debugger, and the current oficial OS X debugger) it enough for all
- * cases, so it's a good starting point.
+ * This struct represents an error.
+ *
+ * error_number is the error as returned by the OS, 0 for no error.
+ * description is a null-terminated string.
  **/
-#define INITIAL_VMMAP_DEPTH 1024
-
-static void print_regions(int pid);
-static void print_regions_with_submaps(int pid);
-static task_t get_task(int pid);
-
-enum mem_search_status {
-    MEM_SEARCH_STATUS_FOUND,
-    MEM_SEARCH_STATUS_NOT_FOUND,
-    MEM_SEARCH_STATUS_PROCESS_NOT_FOUND
+struct error {
+    int error_number;
+    char *description;
 };
 
 /**
- * Searches a sequence of bytes in a mach_region, returing true if found.
- * FIXME(alcuadrado): This should also return the address where found.
+ * This struct represents the result of a memory search operation.
+ *
+ * error may point to a fatal error or be NULL if no such error ocurred.
+ * soft_errors is a dynamic array containing all the non-fatal errors in order.
+ * soft_errors_length is the amount of soft errors in soft_errors.
+ * soft_errors_capacity is the number of errors that fit in soft_errors.
+ * located_at is the address where the pattern was found, or NULL if not found.
+ *
+ * NOTE: This structs must be created and destroyed with
+ *  new_mem_search_return and free_mem_search_return.
  **/
-bool search_bytes_in_region(task_t task, mach_vm_address_t region_start,
-        mach_vm_size_t region_size, char *bytes, size_t bytes_size) {
+struct mem_search_return {
+    struct error *error;
+    struct error *soft_errors;
+    unsigned int soft_errors_count;
+    unsigned int soft_errors_capacity;
+    void *located_at;
+};
 
-    vm_offset_t data;
-    mach_msg_type_number_t data_size;
-    kern_return_t kret;
-    kret = mach_vm_read(task, region_start, region_size, &data, &data_size);
+/**
+ * Allocates a new mem_search_return in the heap and initializes it.
+ **/
+struct mem_search_return *new_mem_search_return(void) {
+    return calloc(1, sizeof(struct mem_search_return));
+}
 
-    if (kret != KERN_SUCCESS) {
-        //FIXME(alcuadrado): Should we propagate this error?
-        //FIXME(alcuadrado): If needed, change the protection.
-        fprintf(stderr, "mach_vm_read failed. error: %x %s\n",
-                    kret, mach_error_string(kret));
-        return false;
+/**
+ * Frees a struct mem_search_return.
+ **/
+void free_mem_search_return(struct mem_search_return *ret) {
+    if (ret == NULL) {
+        return;
     }
 
-    //FIXME(alcuadrado): Implement the actual search here.
+    if (ret->error != NULL) {
+        free(ret->error);
+        ret->error = NULL;
+    }
 
-    vm_deallocate(mach_task_self(), data, data_size);
-    return false;
+    assert((ret->soft_errors_capacity > 0) == (ret->soft_errors != NULL));
+    if (ret->soft_errors != NULL) {
+        free(ret->soft_errors);
+        ret->soft_errors = NULL;
+    }
+
+    ret->located_at = NULL;
+    free(ret);
+}
+
+/**
+ * Sets a mem_search_return's error.
+ **/
+void mem_search_return_set_error(struct mem_search_return *ret,
+        kern_return_t error_number) {
+    if (error_number == KERN_SUCCESS) {
+        return;
+    }
+
+    struct error *error = malloc(sizeof(struct error));
+    error->error_number = error_number;
+    error->description = mach_error_string(error_number);
+    ret->error = error;
+}
+
+/**
+ * Adds a soft error to a mem_search_return.
+ **/
+void mem_search_return_add_soft_error(struct mem_search_return *ret,
+        kern_return_t error_number) {
+    if (error_number == KERN_SUCCESS) {
+        return;
+    }
+
+#define SOFT_ERRORS_INITIAL_CAPACITY 2
+#define SOFT_ERRORS_REALLOCATION_FACTOR 2
+
+    if (ret->soft_errors_capacity == 0) {
+        ret->soft_errors = malloc(
+                sizeof(struct error) * SOFT_ERRORS_INITIAL_CAPACITY);
+        ret->soft_errors_capacity = SOFT_ERRORS_INITIAL_CAPACITY;
+
+    } else if (ret->soft_errors_capacity == ret->soft_errors_count) {
+        ret->soft_errors = realloc(ret->soft_errors,
+                ret->soft_errors_capacity * SOFT_ERRORS_REALLOCATION_FACTOR);
+        ret->soft_errors_capacity *= SOFT_ERRORS_REALLOCATION_FACTOR;
+    }
+
+    ret->soft_errors[ret->soft_errors_count].error_number = error_number;
+    ret->soft_errors[ret->soft_errors_count].description =
+        mach_error_string(error_number);
+    ret->soft_errors_count++;
 }
 
 
 /**
  * Searches for the first occurrence of sequence of bytes in a process' memory
  * starting at initial_address.
- *
- * This function returns a mem_search_status indicating if the bytes where
- * found or not, or if an error ocurred. If the sequence of bytes was found,
- * its initial address (in the process address space) will be available at
- * *found_at.
  **/
-enum mem_search_status memory_search_from_address(int pid, char *bytes,
-        size_t bytes_size, mach_vm_address_t initial_address,
-        mach_vm_address_t *found_at) {
-
+struct mem_search_return * memory_search_from_address(int pid, char *bytes,
+        size_t bytes_size, mach_vm_address_t initial_address) {
     task_t task;
     kern_return_t kret;
+    struct mem_search_return *ret = new_mem_search_return();
 
     kret = task_for_pid(mach_task_self(), pid, &task);
     if (kret != KERN_SUCCESS) {
-        //FIXME(alcuadrado): How should we propagate this error?
-        fprintf(stderr, "task_for_pid failed. error: %x %s\n",
-                    kret, mach_error_string(kret));
-        return MEM_SEARCH_STATUS_PROCESS_NOT_FOUND;
+        mem_search_return_set_error(ret, kret);
+        return ret;
     }
 
     struct vm_region_submap_info_64 info;
     mach_msg_type_number_t info_count = 0;
     mach_vm_address_t addr = initial_address;
     mach_vm_size_t size = 0;
-    uint32_t depth = INITIAL_VMMAP_DEPTH;
+    uint32_t depth = 0;
 
-    while (true) {
+    for (;;) {
         info_count = VM_REGION_SUBMAP_INFO_COUNT_64;
         kret = mach_vm_region_recurse(task,
             &addr, &size, &depth,
@@ -96,11 +148,8 @@ enum mem_search_status memory_search_from_address(int pid, char *bytes,
         }
 
         if (kret != KERN_SUCCESS) {
-            //FIXME(alcuadrado): How should we propagate this error?
-            //Or we can keep searching in the next page.
-            fprintf(stderr, "vm_region failed. error: %x %s\n",
-                    kret, mach_error_string(kret));
-            break;
+            mem_search_return_set_error(ret, kret);
+            return ret;
         }
 
         if(info.is_submap) {
@@ -110,30 +159,57 @@ enum mem_search_status memory_search_from_address(int pid, char *bytes,
 
         printf("Region starting at %p of %llu bytes\n", (void *) addr, size);
 
-        search_bytes_in_region(task, addr, size, bytes, bytes_size);
+        //The actual search should be done here, but it's not a straight forward
+        //by-region search, as we have to search in between regions.
+
         addr += size;
     }
 
-    return MEM_SEARCH_STATUS_NOT_FOUND;
+    return ret;
 }
 
-void try_to_find_and_print(int pid, char *bytes, size_t bytes_size) {
-    mach_vm_address_t found_at;
-    enum mem_search_status ret = memory_search_from_address(pid, bytes,
-        bytes_size, 0, &found_at);
-
-    switch (ret) {
-        case MEM_SEARCH_STATUS_FOUND:
-            printf("Found at: %llu\n", found_at);
-            break;
-        case MEM_SEARCH_STATUS_NOT_FOUND:
-            printf("Not found\n");
-            break;
-        case MEM_SEARCH_STATUS_PROCESS_NOT_FOUND:
-            printf("Process %d not found\n", pid);
-            break;
-    }
-}
+/* #<{(|* */
+/*  * Searches a sequence of bytes in a mach_region, returing true if found. */
+/*  * FIXME(alcuadrado): This should also return the address where found. */
+/*  *|)}># */
+/* bool search_bytes_in_region(task_t task, mach_vm_address_t region_start, */
+/*         mach_vm_size_t region_size, char *bytes, size_t bytes_size) { */
+/*     vm_offset_t data; */
+/*     mach_msg_type_number_t data_size; */
+/*     kern_return_t kret; */
+/*     kret = mach_vm_read(task, region_start, region_size, &data, &data_size); */
+/*  */
+/*     if (kret != KERN_SUCCESS) { */
+/*         //FIXME(alcuadrado): Should we propagate this error? */
+/*         //FIXME(alcuadrado): If needed, change the protection. */
+/*         fprintf(stderr, "mach_vm_read failed. error: %x %s\n", */
+/*                     kret, mach_error_string(kret)); */
+/*         return false; */
+/*     } */
+/*  */
+/*     //FIXME(alcuadrado): Implement the actual search here. */
+/*  */
+/*     vm_deallocate(mach_task_self(), data, data_size); */
+/*     return false; */
+/* } */
+/*  */
+/* void try_to_find_and_print(int pid, char *bytes, size_t bytes_size) { */
+/*     mach_vm_address_t found_at; */
+/*     enum mem_search_status ret = memory_search_from_address(pid, bytes, */
+/*         bytes_size, 0, &found_at); */
+/*  */
+/*     switch (ret) { */
+/*         case MEM_SEARCH_STATUS_FOUND: */
+/*             printf("Found at: %llu\n", found_at); */
+/*             break; */
+/*         case MEM_SEARCH_STATUS_NOT_FOUND: */
+/*             printf("Not found\n"); */
+/*             break; */
+/*         case MEM_SEARCH_STATUS_PROCESS_NOT_FOUND: */
+/*             printf("Process %d not found\n", pid); */
+/*             break; */
+/*     } */
+/* } */
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -143,7 +219,7 @@ int main(int argc, char **argv) {
 
     int pid = atoi(argv[1]);
     char deadbeef[] = {0xd, 0xe, 0xa, 0xd, 0xb, 0xe, 0xe, 0xf};
-    try_to_find_and_print(pid, deadbeef, sizeof(deadbeef));
+    /* try_to_find_and_print(pid, deadbeef, sizeof(deadbeef)); */
 
     return 0;
 }
