570ea5761b88cc520bcd8e1820789f1f78f1ba3e
Marco Vanotti <mvanotti@dc.uba.ar>
2014-09-24 18:44:50 -0300

    [windows] Get All Processes names in c.
    Adds getAllProcesses function that returns all the processes names alongside with their pids.
    Also, a bit of formatting.

--- listlibs/list_libs_windows.h
+++ listlibs/list_libs_windows.h
@@ -5,25 +5,39 @@
 #include <psapi.h>
 
 typedef struct t_EnumProcessesResponse {
-	DWORD error;
-	DWORD *pids;
-	DWORD length;
+    DWORD error;
+    DWORD *pids;
+    DWORD length;
 } EnumProcessesResponse;
 
 typedef struct t_ModuleInfo {
-	char *filename;
-	MODULEINFO info;
+    char *filename;
+    MODULEINFO info;
 } ModuleInfo;
 
 typedef struct t_EnumProcessModulesResponse {
-	DWORD error;
-	DWORD length;
-	ModuleInfo *modules;
+    DWORD error;
+    DWORD length;
+    ModuleInfo *modules;
 } EnumProcessModulesResponse;
 
+typedef struct t_ProcessInfo {
+    DWORD error;
+    char *filename;
+    DWORD pid;
+} ProcessInfo;
+
+typedef struct t_EnumProcessesFullResponse {
+    DWORD error;
+    DWORD length;
+    ProcessInfo *processes;
+} EnumProcessesFullResponse;
+
+EnumProcessesFullResponse *getAllProcesses();
 EnumProcessesResponse *getAllPids();
 EnumProcessModulesResponse *getModules(DWORD pid);
 
+void EnumProcessesFullResponse_Free(EnumProcessesFullResponse *r);
 void EnumProcessesResponse_Free(EnumProcessesResponse *r);
 void EnumProcessModulesResponse_Free(EnumProcessModulesResponse *r);
 
