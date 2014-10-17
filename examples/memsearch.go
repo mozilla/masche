493e2fbe830253fc88fc7bae24f55f9a45dfc3ca
Marco Vanotti <mvanotti@dc.uba.ar>
2014-10-17 13:56:00 -0300

    memsearch example now works with Process interface

--- examples/memsearch.go
+++ examples/memsearch.go
@@ -7,13 +7,13 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/mozilla/masche/memsearch"
 	"log"
 )
 
 var (
 	pid    = flag.Int("pid", 0, "Process id to analyze")
+	prnt   = flag.Bool("print", false, "Print information")
 	needle = flag.String("s", "Find This!", "String to search for")
 	addr   = flag.Int("addr", 0x0, "Process Address to read")
 	size   = flag.Int("n", 4, "Amount of bytes to read")
@@ -22,18 +22,25 @@ var (
 func main() {
 	flag.Parse()
 
-	if *addr != 0 {
-		if err := memsearch.PrintMemory(uint(*pid), uint64(*addr), *size); err != nil {
+	p, err := memsearch.OpenProcess(uint(*pid))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if *prnt {
+		buf := make([]byte, *size)
+		err := p.CopyMemory(uintptr(*addr), buf)
+		if err != nil {
 			log.Fatal(err)
 		}
+		log.Println(string(buf))
 		return
 	}
 
-	if ok, err := memsearch.MemoryGrep(uint(*pid), []byte(*needle)); err != nil {
+	address, found, err := memsearch.FindNext(p, uintptr(*addr), []byte(*needle))
+	if err != nil {
 		log.Fatal(err)
-	} else if ok {
-		fmt.Println("Found")
-	} else {
-		fmt.Println("Not Found")
+	} else if found {
+		log.Printf("Found in address: %x\n", address)
 	}
 }
