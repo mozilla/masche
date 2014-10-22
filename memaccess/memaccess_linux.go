6e7f3e7ab01aa9222045a45b19a53d7574aa9893
Laski <laski.nahuel@gmail>
2014-10-22 14:06:34 -0400

    Fixed imports

--- memaccess/memaccess_linux.go
+++ memaccess/memaccess_linux.go
@@ -132,6 +132,7 @@ func (p process) ReadMemory(address uintptr, size uint) ([]byte, error) {
 func (p process) CopyMemory(address uintptr, buffer []byte) error {
 	mem, err := os.Open(p.memFilepath)
 	if err != nil {
+		//TODO(laski): add address to error string
 		return err
 	}
 	defer mem.Close()
