7c8f6d3f2b54260943219523db3e391843865ddc
Marco Vanotti <mvanotti@dc.uba.ar>
2015-02-08 19:30:52 -0800

    [process/windows] Implement GetAllPids and Name()
    Both functions have been extracted from listlibs. In a later commit I
    will clean up all the unused code from that library.

--- process/process_windows.c
+++ process/process_windows.c
@@ -1,12 +1,13 @@
 #include "process.h"
+#include "process_windows.h"
 
 response_t *open_process_handle(pid_tt pid, process_handle_t *handle) {
     response_t *res = response_create();
 
     *handle = (uintptr_t) OpenProcess(PROCESS_QUERY_INFORMATION |
-                                      PROCESS_VM_READ,
-                                      FALSE,
-                                      pid);
+            PROCESS_VM_READ,
+            FALSE,
+            pid);
 
     if (*handle == 0) {
         res->fatal_error = error_create(GetLastError());
@@ -23,5 +24,68 @@ response_t *close_process_handle(process_handle_t process_handle) {
         res->fatal_error = error_create(GetLastError());
     }
 
+    return res;
+}
+
+EnumProcessesResponse *getAllPids() {
+    DWORD size = sizeof(DWORD) * 512;
+    DWORD *aProcesses = NULL;
+    DWORD cbNeeded;
+    EnumProcessesResponse *res = calloc(1, sizeof * res);
+    // EnumProcesses modifies cbNeeded, setting it to the amount of bytes
+    // written into aProcesses. Thus, we need to check if cbNeeded is equal
+    // to size. In that case, it means that the array was filled completely and
+    // we need to use a bigger array because probably we left elements out.
+    do {
+        size *= 2;
+        aProcesses = realloc(aProcesses, size);
+        BOOL success = EnumProcesses(aProcesses, size, &cbNeeded);
+        if (!success) {
+            res->error = GetLastError();
+            free(aProcesses);
+            return res;
+        }
+    } while (cbNeeded == size);
+    res->error = 0;
+    res->pids = aProcesses;
+    res->length = cbNeeded / sizeof(DWORD);
+    return res;
+}
+
+void EnumProcessesResponse_Free(EnumProcessesResponse *r) {
+    if (r == NULL) {
+        return;
+    }
+    free(r->pids);
+    free(r);
+}
+
+response_t *GetProcessName(process_handle_t hndl, char **name) {
+    response_t *res = response_create();
+    DWORD bufSize = 128;
+    char *buf = calloc(bufSize, sizeof * buf);
+    //TODO(mvanotti): GetProcessImageFileName returns the name in "Device Format"
+    // ej. \Device\HardDiskVolume4\Windows\foo.exe We can use another function to get it
+    // in "normal" format ( C:\Windows\foo.exe )
+    DWORD r;
+    do {
+        r = GetProcessImageFileName((HANDLE) hndl, buf, bufSize);
+        if (r == 0) {
+            DWORD error = GetLastError();
+            // This is the only error that we know how to handle:
+            // use a bigger buffer.
+            if (error == ERROR_INSUFFICIENT_BUFFER) {
+                bufSize *= 2;
+                buf = realloc(buf, bufSize);
+            } else {
+                free(buf);
+                res->fatal_error = error_create(error);
+                return res;
+            }
+        }
+    } while (r == 0);
+
+    *name = _strdup(buf);
+    free(buf);
     return res;
 }
--- process/process_windows.go
+++ process/process_windows.go
@@ -1,11 +1,50 @@
 package process
 
-import "errors"
+// #cgo CFLAGS: -std=c99
+// #cgo CFLAGS: -DPSAPI_VERSION=1
+// #cgo LDFLAGS: -lpsapi
+// #include "process.h"
+// #include "process_windows.h"
+import "C"
+
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+
+	"github.com/mozilla/masche/cresponse"
+)
 
 func (p process) Name() (name string, harderror error, softerrors []error) {
-	return "", errors.New("NOT YET IMPLEMENTED"), nil
+	var cname uintptr
+	r := C.GetProcessName(p.hndl, (**C.char)(unsafe.Pointer(&cname)))
+
+	harderror, softerrors = cresponse.GetResponsesErrors(unsafe.Pointer(r))
+	C.response_free(r)
+	if harderror != nil {
+		name = C.GoString((*C.char)(unsafe.Pointer(cname)))
+		C.free(unsafe.Pointer(cname))
+	}
+	return
 }
 
 func getAllPids() (pids []uint, harderror error, softerrors []error) {
-	return nil, errors.New("NOT YET IMPLEMENTED"), nil
+	r := C.getAllPids()
+	defer C.EnumProcessesResponse_Free(r)
+	if r.error != 0 {
+		return nil, fmt.Errorf("getAllPids failed with error %d", r.error), nil
+	}
+
+	pids = make([]uint, r.length)
+	// We use this to access C arrays without doing manual pointer arithmetic.
+	cpids := *(*[]C.DWORD)(unsafe.Pointer(
+		&reflect.SliceHeader{
+			Data: uintptr(unsafe.Pointer(r.pids)),
+			Len:  int(r.length),
+			Cap:  int(r.length)}))
+	for i, _ := range pids {
+		pids[i] = uint(cpids[i])
+	}
+
+	return pids, nil, nil
 }
