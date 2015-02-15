aa69a8c991ee7d6537a21d2e9bf599a132f7b6d3
Marco Vanotti <mvanotti@dc.uba.ar>
2015-02-14 20:58:55 -0800

    [examples/listlibs] update example

--- examples/listlibs.go
+++ examples/listlibs.go
@@ -10,6 +10,7 @@ import (
 	"regexp"
 
 	"github.com/mozilla/masche/listlibs"
+	"github.com/mozilla/masche/process"
 )
 
 var rstr = flag.String("r", "", "library name regexp")
@@ -22,17 +23,49 @@ func main() {
 		log.Fatal(err)
 	}
 
-	procs, softerrs, err := listlibs.FindProcWithLib(r)
-	if err != nil {
-		log.Fatal(err)
+	ps, hard, softs := process.OpenAll()
+	if hard != nil {
+		log.Fatal(hard)
+	}
+	defer process.CloseAll(ps)
+	for _, e := range softs {
+		log.Println(e)
 	}
 
-	for _, e := range softerrs {
-		fmt.Println(e.Error())
+	matches, hard, softs := findProcWithLib(r, ps)
+	if hard != nil {
+		log.Fatal(hard)
+	}
+	for _, e := range softs {
+		fmt.Println(e)
 	}
 
-	for _, p := range procs {
-		fmt.Println(p)
+	fmt.Printf("Processes matching: %s\n", *rstr)
+	for p, libs := range matches {
+		n, hard, _ := p.Name()
+		if hard != nil {
+			log.Fatal(hard)
+		}
+		fmt.Printf("[%d] %s\n", p.Pid(), n)
+		for _, l := range libs {
+			fmt.Printf("\t%s\n", l)
+		}
 	}
 
+}
+
+func findProcWithLib(r *regexp.Regexp, ps []process.Process) (matches map[process.Process][]string, harderror error, softerrors []error) {
+	matches = make(map[process.Process][]string)
+	softerrors = make([]error, 0)
+	for _, p := range ps {
+		libs, hard, softs := listlibs.GetMatchingLoadedLibraries(p, r)
+		if hard != nil {
+			return nil, hard, softerrors
+		}
+		softerrors = append(softerrors, softs...)
+		if len(libs) != 0 {
+			matches[p] = libs
+		}
+	}
+	return matches, nil, softerrors
 }
